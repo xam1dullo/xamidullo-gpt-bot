7bcc425ee20501d3901e6e4c3e992b1cf8431ec0
khamidullo <khkhamidullo@gmail.com>
2023-12-02 22:28:22 +0500

    update package

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
-export * from "https://deno.land/x/grammy@v1.17.0/mod.ts";
+export * from "https://deno.land/x/grammy@v1.18.1/mod.ts";
+
 export { serve } from "https://deno.land/std@0.208.0/http/mod.ts";
